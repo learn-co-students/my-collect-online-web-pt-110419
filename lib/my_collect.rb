bfc0cebdb2a1dc7c261b52264e7616056bd62637
Antony Sanders <antony.devon92@gmail.com>
2019-11-15 00:12:53 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+def my_collect(array)
+    counter = 0
+    new_array = []
+ 
+    while counter < array.length
+      yield(array[counter])
+      counter = counter + 1
+      new_array << array.split(" ").first
+    end
+    new_array
+end
 
