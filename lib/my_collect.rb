543e84aa42e817f4055c6f9edd2b02d459eb7b4d
kenneth gil <astronori@gmail.com>
2019-11-30 17:46:50 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+nicknames = ["Kenny G", "Astronori", "K-Tut"]
+def my_collect(array)
+  i = 0 
+  name_collection = []
+while i < array.length
+  name_collection.push yield (array[i])
+  i += 1
+end
+name_collection
+end
+my_collect(nicknames) {|i| i.split(" ").first}
